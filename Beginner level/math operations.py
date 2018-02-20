393f1cfb7e2595fcd0780bc043a64b0ee24aa5fb
kishorekris1319 <36623662+kishorekris1319@users.noreply.github.com>
2018-02-20 11:14:39 +0530

    Create math operations.py

---
+++ Beginner level/math operations.py
@@ -0,0 +1,21 @@
+#python code goes here
+#python version :3 
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
+f = 6
+g = 7
+h = 8
+i = 9
+j = 1
+print (h+i)
+print (g-f)
+print (c*d)
+print (d/b)
+print (e%c)
+print (b**b)
+print (f<>g)
+print (id(a))
+print (g//b)
